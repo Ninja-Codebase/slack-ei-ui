b13f36558ad8279c1219edd46bc84db20ae9d0ef
nauhil <nauhil.web@gmail.com>
2026-06-18 19:39:06 +0800

    Update Dashboard Components

--- src/app/(main)/dashboard/default/page.tsx
+++ src/app/(main)/dashboard/default/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="@container/main flex flex-col gap-4 md:gap-6">
       <MetricCards />
-      <PerformanceOverview />
+      {/* <PerformanceOverview /> */}
       <SubscriberOverview />
     </div>
   );
