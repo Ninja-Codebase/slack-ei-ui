7a2684886e61890fc5f0129edb55e42e16a53e5c
nauhil <nauhil.web@gmail.com>
2026-06-18 19:12:46 +0800

    Add Configuration Tab and Update Name

--- src/navigation/sidebar/sidebar-items.ts
+++ src/navigation/sidebar/sidebar-items.ts
@@ -14,6 +14,7 @@ import {
   Mail,
   MessageSquare,
   ReceiptText,
+  Settings,
   ShoppingBag,
   SquareArrowUpRight,
   Users,
@@ -50,7 +51,12 @@ export const sidebarItems: NavGroup[] = [
     label: "Dashboards",
     items: [
       {
-        title: "Default",
+        title: "Configuration",
+        url: "/dashboard/crm",
+        icon: Settings,
+      },
+      {
+        title: "Dashboard",
         url: "/dashboard/default",
         icon: LayoutDashboard,
       },
